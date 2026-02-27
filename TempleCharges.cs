65a5ad1eb21cf165f84ab63b4876a5833dc98b58
beanZ9 <248049379+beanZ9@users.noreply.github.com>
2026-02-27 16:24:07 +0100

    add turn in hint; show amount gained recently

--- TempleCharges.cs
+++ TempleCharges.cs
@@ -1,6 +1,7 @@
 using ExileCore2;
 using ExileCore2.PoEMemory.Elements;
 using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 
@@ -10,8 +11,17 @@ public class TempleCharges : BaseSettingsPlugin<TempleChargesSettings>
 {
     private bool _initialized = false;
     private TempleConsolePanel _templePanel;
-    private string _output;
+
+    private int _chargesCount;
+    private int _maxCharges;
+    private int _previousChargesCount = -1;
+    private int _recentlyGained;
     private bool _chargesFull;
+    private bool _areaIsTemple;
+    private bool _canGrabRooms;
+
+    private Stopwatch _notificationTimer = new Stopwatch();
+    private string _output;
 
     public override bool Initialise() {
         _initialized = true;
@@ -20,6 +30,7 @@ public class TempleCharges : BaseSettingsPlugin<TempleChargesSettings>
 
     public override void AreaChange(AreaInstance area) {
         _templePanel = GameController.IngameState.IngameUi.TempleConsolePanel;
+        _areaIsTemple = area.Area.Id.Equals("IncursionTemple");
     }
 
     public override void Tick() {
@@ -31,7 +42,8 @@ public class TempleCharges : BaseSettingsPlugin<TempleChargesSettings>
             return;
         }
 
-        var chargesText = "N/A";
+        _output = "Temple Crystals: ";
+        var chargesText = string.Empty;
 
         try {
             var rawString = _templePanel.Children[8].Children[1].Children[0].Text;
@@ -39,14 +51,45 @@ public class TempleCharges : BaseSettingsPlugin<TempleChargesSettings>
             chargesText = rawString.Split(' ').LastOrDefault();
             LogDebug($"Charges text: {chargesText}");
         } catch (Exception ex) {
-            chargesText += " (Open Temple Console)";
+            _output += "N/A (Open Temple Console)";
             LogDebug($"Exception getting charges text: {ex.Message}");
+            return;
         }
 
         var charges = chargesText.Split('/');
-        _chargesFull = charges.Length > 1 && charges[0].Equals(charges[1]);
 
-        _output = $"Temple Crystals: {chargesText}";
+        if (_previousChargesCount >= 0) {
+            _previousChargesCount = _chargesCount;
+        }
+        
+        try {
+            _chargesCount = int.Parse(charges[0]);
+            _maxCharges = int.Parse(charges[1]);
+        } catch (Exception ex) {
+            LogDebug($"Exception parsing charges: {ex.Message}");
+        }
+
+        if (_previousChargesCount < 0) {
+            _previousChargesCount = _chargesCount;
+        }
+
+        _chargesFull = charges.Length > 1 && _chargesCount >= _maxCharges;
+
+        try {
+            _canGrabRooms = _templePanel.Children[9].Children[2].ChildCount <= 0 &&
+                 _chargesCount >= 6 &&
+                !(_templePanel.Children[8].Children[0].Children[0].Text.Contains("close temple") ||
+                    _areaIsTemple);
+        } catch (Exception ex) {
+            LogDebug($"Exception getting room cards: {ex.Message}");
+        }
+
+        _output += $"{_chargesCount}/{_maxCharges}{(Settings.ShowActive && _canGrabRooms ? $" (Turn in)" : string.Empty)}";
+        
+        if (_previousChargesCount < _chargesCount) {
+            _recentlyGained = _chargesCount - _previousChargesCount;
+            _notificationTimer.Restart();
+        }
     }
 
     public override void Render() {
@@ -57,12 +100,21 @@ public class TempleCharges : BaseSettingsPlugin<TempleChargesSettings>
             return;
         }
         
+        if (_notificationTimer.IsRunning) {
+            if (_notificationTimer.ElapsedMilliseconds < 10000) {
+                _output += $"\nRecently gained: {_recentlyGained}";
+            } else {
+                _notificationTimer.Reset();
+            }
+        }
+
         var size = Graphics.MeasureText(_output);
-        var color = _chargesFull ? Color.Red : Color.White;
+        var color = _chargesFull ? Color.Red : (_canGrabRooms ? Color.Green : Color.White);
 
         Graphics.DrawBox(new ExileCore2.Shared.RectangleF(Settings.PositionX - 5, Settings.PositionY, size.X + 10, size.Y), Settings.BackgroundColor);
         Graphics.DrawText(_output, new System.Numerics.Vector2(Settings.PositionX, Settings.PositionY), color);
     }
+
 
     private void LogDebug(string msg) {
         if (Settings.Debug) {
--- TempleChargesSettings.cs
+++ TempleChargesSettings.cs
@@ -1,4 +1,5 @@
-﻿using ExileCore2.Shared.Interfaces;
+﻿using ExileCore2.Shared.Attributes;
+using ExileCore2.Shared.Interfaces;
 using ExileCore2.Shared.Nodes;
 using System.Drawing;
 
@@ -8,6 +9,8 @@ public class TempleChargesSettings : ISettings
 {
     //Mandatory setting to allow enabling/disabling your plugin
     public ToggleNode Enable { get; set; } = new ToggleNode(false);
+    [Menu("Show Hint", "Show a hint when 6 of your current crystals can be traded in and converted to a set of rooms.\nYou can store 6 addtional charges this way for a total of 66.")]
+    public ToggleNode ShowActive { get; set; } = new ToggleNode(true);
     public RangeNode<int> PositionX { get; set; } = new RangeNode<int>(1800, 0, 10000);
     public RangeNode<int> PositionY { get; set; } = new RangeNode<int>(100, 0, 10000);
     public ColorNode BackgroundColor { get; set; } = new ColorNode() { Value = Color.FromArgb(128, 0, 0, 0)};
